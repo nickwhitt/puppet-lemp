5bb8058d169838bfe5576bee2d1ee3fa8cf6a7c6
Nick Whitt <nick.whitt@gmail.com>
2015-06-09 23:54:50 -0400

    Using Hiera automatic lookup for class parameters
    Closes #4

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,13 +1,13 @@
 hiera_include('classes')
 
 nginx::resource::vhost { 'vagrant.app':
-  www_root => hiera('nginx::www_root', '/vagrant'),
+  www_root => '/vagrant',
 }
 
 nginx::resource::location { 'vagrant_root':
   ensure        => present,
-  vhost         => hiera('nginx::vhost', 'vagrant.app'),
-  www_root      => hiera('nginx::www_root', '/vagrant'),
+  vhost         => 'vagrant.app',
+  www_root      => '/vagrant',
   location      => '~ \.php$',
   index_files   => ['index.php', 'index.html', 'index.htm'],
   fastcgi       => 'unix:/var/run/php5-fpm.sock',
@@ -16,26 +16,7 @@ nginx::resource::location { 'vagrant_root':
   },
 }
 
-class { 'timezone':
-    timezone => hiera('timezone', 'UTC'),
-}
-
-class { '::mysql::server':
-  package_name            => 'mariadb-server',
-  root_password           => hiera('mysql::root_password', 'secret'),
-  remove_default_accounts => hiera('mysql::remove_default_accounts', true),
-}
-
-class { '::mysql::client':
-  package_name => 'mariadb-client',
-}
-
-class { '::mysql::bindings':
-  php_enable => 'true',
-}
-
 mysql::db { 'app':
-  dbname   => hiera('mysql::db', 'app'),
-  user     => hiera('mysql::user', 'vagrant'),
-  password => hiera('mysql::password', 'vagrant'),
+  user     => 'vagrant',
+  password => 'vagrant',
 }
